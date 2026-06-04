67ce4db951811bb64a05b868d8e706c54c2caba6
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-06-04 12:03:33 +0100

    Add echo progress messages to deploy-nginx script

--- scripts/deploy-nginx.sh
+++ scripts/deploy-nginx.sh
@@ -2,9 +2,12 @@
 
 # Update and download latest version of packages
 sudo apt update -y
+echo "✔ Package list updated"
 
 # Install latest versions of packages
 sudo apt upgrade -y
+echo "✔ Packages upgraded"
 
 # Install and run nginx web server
 sudo apt install nginx -y
+echo "✔ Nginx installed and running"
